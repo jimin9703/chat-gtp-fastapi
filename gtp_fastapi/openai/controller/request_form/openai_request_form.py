e546aeea1bae3fc956d120ddb319cb0f9ce8b232
jimin9703 <moses0322@naver.com>
2024-08-19 16:36:46 +0900

    init:[김지민] DLLS를 위한 초기화[GTP-FASTAPI]

--- gtp_fastapi/openai/controller/request_form/openai_request_form.py
+++
@@ -1,5 +0,0 @@
-from pydantic import BaseModel
-
-
-class OpenAITalkRequestForm(BaseModel):
-    userInput: str
--- gtp_fastapi/openai/service/openai_service.py
+++
@@ -1,7 +0,0 @@
-from abc import ABC, abstractmethod
-
-
-class OpenAIBasicService(ABC):
-    @abstractmethod
-    def testai(self, userInput):
-        pass
