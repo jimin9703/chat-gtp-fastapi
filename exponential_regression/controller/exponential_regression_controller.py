24e8f78931866ae3dd3cdd88a7e7ba156ca808f5
jimin9703 <moses0322@naver.com>
2024-08-19 16:34:17 +0900

    init:[김지민] DLLS를 위한 초기화[GTP-FASTAPI]

---
+++ exponential_regression/controller/exponential_regression_controller.py
@@ -0,0 +1,14 @@
+from fastapi import APIRouter, Depends
+
+from exponential_regression.service.exponential_regression_service_impl import ExponentialRegressionServiceImpl
+
+exponentialRegressionRouter = APIRouter()
+
+async def injectExponentialRegressionService() -> ExponentialRegressionServiceImpl:
+    return ExponentialRegressionServiceImpl()
+
+
+@exponentialRegressionRouter.get("/exponential-regression")
+def exponentialRegression(exponentialRegressionService: ExponentialRegressionServiceImpl = Depends(injectExponentialRegressionService)):
+    print(f"exponentialRegression()")
+    return exponentialRegressionService.createSampleForExponentialRegression()
